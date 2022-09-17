01266f913bef6137865d52a0855aa6c966862c1d
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-09-17 10:24:29 +0300

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :user_id
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
 
   def last_five_comments
     comments.last(5)
