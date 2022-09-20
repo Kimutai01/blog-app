f815d02bce2049b784e7ec4187e48e0834930c98
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-09-20 11:53:22 +0300

    Added controller testing

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users Page', type: :request do
+  describe 'GET /users/index' do
+    before(:example) { get '/users' }
+    it 'the path works' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'displays the placeholder text' do
+      expect(response.body).to include('This is the users page')
+    end
+  end
+
+  describe 'GET /users/show' do
+    before(:example) { get '/users/1' }
+    it 'the path works' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'displays the placeholder text' do
+      expect(response.body).to include('This is a single user page')
+    end
   end
 end
