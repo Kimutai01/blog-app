ad9a06bb633e98099bf7655c7dafd87c8fa16da3
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-09-22 18:50:22 +0300

    Styling user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.where(user_id: params[:user_id])
-    @author = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   def show
